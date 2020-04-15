5e2905948908678b6f57ab68fd07ad736a0d0a35
infinitelytight <6633057+infinitelytight@users.noreply.github.com>
2020-04-15 12:45:13 +0100

    Create Cabal project

--- Game.hs
+++ Game.hs
@@ -6,8 +6,7 @@ import Hand
 p1Hand = readHand ["8C", "8S", "8C", "8H", "2S"]
 p2Hand = readHand ["2D", "2S", "2C", "2H", "AC"]
 
-game = compareHands p1Hand p2Hand
-main = print game
+game = print $ compareHands p1Hand p2Hand
 
 -- TODO
 -- use assoc to `find` hands
